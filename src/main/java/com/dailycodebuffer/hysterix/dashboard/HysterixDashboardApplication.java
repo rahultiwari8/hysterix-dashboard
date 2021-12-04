483491de32b72ff921b4bf535dc101b3c2a4830e
Rahul Tiwari <rahtiwari@gmail.com>
2021-12-04 22:18:28 +0530

    Initial commit

---
+++ src/main/java/com/dailycodebuffer/hysterix/dashboard/HysterixDashboardApplication.java
@@ -0,0 +1,17 @@
+package com.dailycodebuffer.hysterix.dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@SpringBootApplication
+@EnableHystrixDashboard
+@EnableEurekaClient
+public class HysterixDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HysterixDashboardApplication.class, args);
+	}
+
+}
